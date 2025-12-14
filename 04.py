08e7b7c1af5cd9269c5c9276463b04c787168a17
Waaberi Ibrahim <wibra007@uottawa.ca>
2025-12-14 13:17:49 -0500

    cleaning up useless code from day 4

--- 04.py
+++ 04.py
@@ -7,30 +7,6 @@ def proper_check_adj_pos(lst: list[list[str]], i: int, j: int) -> int:
             ans += lst[a][b] == "@"
     return ans
 
-
-def solver_lazy(dat: list[list[str]]) -> tuple[list[list[str]], int]:
-    ans = 0
-    cpy = [i[:] for i in dat]
-    for i in range(len(dat)):
-        for j in range(len(dat[i])):
-            if dat[i][j] == "@" and proper_check_adj_pos(dat, i, j) < 4:
-                ans += 1
-                cpy[i][j] = "x"
-    return cpy, ans
-
-def part2_lazy(inp: str) -> int:
-    dat = [list(i) for i in inp.splitlines()]
-    total = 0
-    dat, summary = solver_lazy(dat)
-    while summary != 0:
-        #print(summary)
-        total += summary
-        dat, summary = solver_lazy(dat)
-    return total
-
-def part1(inp: str) -> str | int | None:
-    return solver([list(i) for i in inp.splitlines()])
-
 def solver(dat: list[list[str]], modify: bool = False):
     ans = 0
     for i in range(len(dat)):
@@ -39,6 +15,9 @@ def solver(dat: list[list[str]], modify: bool = False):
                 ans += 1
                 if modify: dat[i][j] = "x"
     return ans
+
+def part1(inp: str) -> str | int | None:
+    return solver([list(i) for i in inp.splitlines()])
 
 def part2(inp: str) -> str | int | None:
     dat = [list(i) for i in inp.splitlines()]
@@ -49,7 +28,7 @@ def part2(inp: str) -> str | int | None:
     return total
 
 
-aoc = AoC(part_1=part1, part_2=part2_lazy)
+aoc = AoC(part_1=part1, part_2=part2)
 inp = """..@@.@@@@.
 @@@.@.@.@@
 @@@@@.@.@@
